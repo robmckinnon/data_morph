a2de9d3bda2b1378ba3ca613767c1170607911a7
robmckinnon <rob@movingflow>
2016-07-29 17:48:51 +0100

    Add project description and package details

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule DataMorph.Mixfile do
     [app: :data_morph,
      version: "0.0.1",
      elixir: "~> 1.3",
+     description: description,
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
@@ -19,6 +21,20 @@ defmodule DataMorph.Mixfile do
       {:csv, "~> 1.4.2"},
       {:mix_test_watch, "~> 0.2", only: :dev},
       {:parallel_stream, "~> 1.0.5"},
+    ]
+  end
+
+  defp description do
+    """
+    Create Elixir structs from data
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Rob McKinnon"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/robmckinnon/data_morph" },
     ]
   end
 end
