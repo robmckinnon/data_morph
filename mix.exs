e6b5f39a4b26006ef952e76e0bfdbe5a1e9db3cc
robmckinnon <rob@movingflow>
2018-06-26 22:17:16 +0100

    Configure CI Elixir versions

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule DataMorph.Mixfile do
     [
       app: :data_morph,
       version: @version,
-      elixir: "~> 1.2 or ~> 1.3 or ~> 1.4 or ~> 1.5 or ~> 1.6",
+      elixir: "~> 1.3 or ~> 1.4 or ~> 1.5 or ~> 1.6",
       description: description(),
       deps: deps(),
       package: package(),
